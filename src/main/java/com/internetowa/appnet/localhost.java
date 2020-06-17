64bacc12be91f8542718b480d16d30a548a3165a
Damtro3 <50495859+Damtro3@users.noreply.github.com>
2020-06-17 14:30:40 +0200

    Update localhost.java

--- src/main/java/com/internetowa/appnet/localhost.java
+++ src/main/java/com/internetowa/appnet/localhost.java
@@ -1,25 +1,18 @@
 package com.internetowa.appnet;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Date;
 
 @RestController
 public class localhost {
 
     @GetMapping
-    public Date czas()
+    public String czas()
     {
-        String date_s = " 2011-01-18 00:00:00";
         Date date = new Date();
-        SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(dt1.format(date));
-        return date;
+        SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        return dt1.format(date);
     }
 }
