7a1c76789772f46900bf9a122746424a9ec7cfb5
Damtro3 <50495859+Damtro3@users.noreply.github.com>
2020-06-18 21:59:30 +0200

    delete

--- src/main/java/com/internetowa/appnet/MainWindowController.java
+++
@@ -1,7 +0,0 @@
-package com.internetowa.appnet;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class MainWindowController {
-}
--- src/main/java/com/internetowa/appnet/SpringConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.internetowa.appnet;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpringConfiguration {
-}
