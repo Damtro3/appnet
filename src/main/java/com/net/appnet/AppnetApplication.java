a8ba637f5d600ef9853bac50f290ae6060c67bc7
Damtro3 <50495859+Damtro3@users.noreply.github.com>
2020-06-19 09:29:35 +0200

    appnet

---
+++ src/main/java/com/net/appnet/AppnetApplication.java
@@ -0,0 +1,13 @@
+package com.net.appnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/net/appnet/AppnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.appnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
