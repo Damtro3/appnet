7bf399540a371a91881d7a553f8bd2d271a18b87
Damtro3 <50495859+Damtro3@users.noreply.github.com>
2020-06-19 16:09:42 +0200

    2

---
+++ src/main/java/com/net/appnet/WebController.java
@@ -0,0 +1,25 @@
+package com.net.appnet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+
+
+    @RequestMapping("/test")
+    public String test()
+    {
+        System.out.println("jestem tutaj");
+        return "index";
+    }
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        System.out.println("jestem tutaj");
+        return "hello";
+    }
+
+}
